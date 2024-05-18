960a47af81846abe673726587d93a2527c3c893d
Lemorphic <antoine.lemor@umontreal.ca>
2024-05-18 11:48:47 -0400

    update

--- Results/Database.R
+++ Results/Database.R
@@ -16,15 +16,17 @@ library(RColorBrewer)
 library(readxl)
 
 
+#### CRÉATION DE LA BASE DE DONNÉES POUR RÉGRESSION OLS ####
+
+
 data <- read.csv(file.path(import_data_path, "annotated_speech_texts.csv"))
 
-# Assurer que la date est au bon format
+# Format date
 data$date <- as.Date(data$date)
 
 # Préparation des données avec total_sentences conservé par discours
-# Cette fois, incluons directement la colonne 'date' dans 'data_prepared'
 data_prepared <- data %>%
-  group_by(doc_ID, date, intervenant) %>%  # Inclure 'date' dans le group_by
+  group_by(doc_ID, date, intervenant) %>%  
   mutate(total_sentences = n()) %>%
   ungroup()
 
@@ -36,7 +38,6 @@ data_time_series <- data_prepared %>%
   summarise(count = n(), total_sentences = first(total_sentences), .groups = "drop") %>%
   mutate(proportion = count / total_sentences) %>%
   ungroup() %>%
-  # Nettoyer les noms des variables pour l'affichage
   mutate(variable_clean = gsub("^detect_", "", variable),
          variable_clean = gsub("_", " ", variable_clean),
          variable_clean = tools::toTitleCase(variable_clean))
@@ -49,11 +50,11 @@ labels_data <- data_time_series %>%
   ungroup()
 
 
-## FAR RIGHT SCORE PAR PREMIER(E) MINISTRE ##
+# Calcul SIED
 
 data <- read.csv(file.path(import_data_path, "annotated_speech_texts.csv"))
 
-# Assurez-vous que data$date est au bon format
+# Format date
 data$date <- as.Date(data$date)
 
 # Convertir les valeurs 'oui'/'non' en valeurs numériques
@@ -61,7 +62,7 @@ data <- data %>%
   mutate(across(starts_with("detect_") | ends_with("_1") | ends_with("_2") | ends_with("_3") | ends_with("_4") | ends_with("_5") | ends_with("_6"), 
                 ~ ifelse(. == "oui", 1, 0)))
 
-# Simplification du calcul du score d'extrême droite
+# Calcul du SIED
 score_data_date <- data %>%
   pivot_longer(cols = starts_with("detect_"), names_to = "detect_type", values_to = "detect_presence") %>%
   filter(!(detect_type %in% c('detect_ecologie', 'detect_tech', 'detect_ue', 'detect_prodem', 'detect_soc', 'detect_travail'))) %>%
@@ -79,10 +80,10 @@ score_data_date <- score_data_date %>%
   mutate(intervenant_date = paste(intervenant, format(date, "%Y-%m-%d")))
 
 
-## NÉGATIVITÉ DU DISCOURS ##
+# Indice de négativité du discours
 data <- read.csv(file.path(import_data_path, "annotated_speech_texts.csv"))
 
-# Assurez-vous que data$date est au bon format
+# Format date
 data$date <- as.Date(data$date)
 
 # Calcul de l'indice de négativité
@@ -94,13 +95,13 @@ data_negatif <- data %>%
   mutate(indice_negatif = (negatif_count / total_phrases) * 100) %>%
   ungroup()
 
-## POSITIVITÉ DU DISCOURS ##
+# Indice de positivité du discours
 data <- read.csv(file.path(import_data_path, "annotated_speech_texts.csv"))
 
 # Assurez-vous que data$date est au bon format
 data$date <- as.Date(data$date)
 
-# Calcul de l'indice de négativité
+# Calcul de l'indice de positivité
 data_positif <- data %>%
   group_by(doc_ID, intervenant, date) %>%
   summarise(total_phrases = n(),
@@ -111,13 +112,13 @@ data_positif <- data %>%
 
 
 
-# Charger les données Excel
-
+# Charger les données électorales
 excel_data <- read_excel("/Users/antoine/Documents/GitHub/FR.POL.GEN/FR.POL.GEN/Database/PM.xlsx")
-# Assurez-vous que data$date est au bon format
+
+# Format date
 excel_data$date <- as.Date(excel_data$date)
 
-# Pivot de data_time_series pour chaque thématique en colonne
+# Pivot de data_time_series pour chaque CIAED en colonnes
 data_themes_pivoted <- data_time_series %>%
   mutate(variable_clean = gsub("^detect_", "", variable),
          variable_clean = gsub("_", " ", variable_clean),
@@ -125,13 +126,13 @@ data_themes_pivoted <- data_time_series %>%
   select(date, intervenant, variable_clean, proportion) %>%
   pivot_wider(names_from = variable_clean, values_from = proportion, values_fill = list(proportion = 0))
 
-# Joindre les scores d'extrême droite, et indices de positivité/négativité
+# Joindre le SIED et les autres indices
 FR_POL_GEN_database <- data_themes_pivoted %>%
   left_join(select(score_data_date, date, intervenant, far_right_score), by = c("date", "intervenant")) %>%
   left_join(select(data_negatif, date, intervenant, indice_negatif), by = c("date", "intervenant")) %>%
   left_join(select(data_positif, date, intervenant, indice_positif), by = c("date", "intervenant"))
 
-# Joindre les données Excel
+# Joindre les données électorales
 FR_POL_GEN_database <- left_join(FR_POL_GEN_database, excel_data, by = c("date", "intervenant"))
 
 # Exporter la base de données finale
@@ -143,7 +144,7 @@ write.csv(FR_POL_GEN_database, file.path(export_path, "FR.POL.GEN_database.csv")
 
 
 
-## TABLEAU DE CORRELATION ##
+#### MATRICE DE CORRELATION ####
 
 # Chargement des bibliothèques nécessaires
 library(readr)  # Pour la fonction read_csv
@@ -155,10 +156,10 @@ library(reshape2)  # Pour la transformation des données
 input_file <- file.path(export_path, "FR.POL.GEN_database.csv")
 reg_data_daily <- read_csv(input_file)
 
+# Normalisation des variables
 reg_data_daily$MP_total <- (reg_data_daily$MP_total - min(reg_data_daily$MP_total)) / (max(reg_data_daily$MP_total) - min(reg_data_daily$MP_total)) * 100
 reg_data_daily$MP_E_D <- (reg_data_daily$MP_E_D - min(reg_data_daily$MP_E_D)) / (max(reg_data_daily$MP_E_D) - min(reg_data_daily$MP_E_D)) * 100
 reg_data_daily$MP_center <- (reg_data_daily$MP_center - min(reg_data_daily$MP_center)) / (max(reg_data_daily$MP_center) - min(reg_data_daily$MP_center)) * 100
-
 
 # Vérifiez que les noms de variables dans 'reg_data_daily' sont corrects
 print(colnames(reg_data_daily))
@@ -169,17 +170,16 @@ cor_data <- reg_data_daily %>%
   select(all_of(variables)) %>% 
   cor(use = "complete.obs")  # Calcule la corrélation en excluant les NA
 
-# Renommez les variables dans la matrice de corrélation
+# Renommer les variables 
 colnames(cor_data) <- c("Score présidentiel", "Score Premier Ministre", "Score Extrême Droite", "Total Députés", "Députés Extrême Droite", "Députés Centre")
 rownames(cor_data) <- colnames(cor_data)
 
 # Sauvegarde de la matrice de corrélation renommée
-write.csv(cor_data, file.path(export_path, "QC.cor_data_renamed.csv"), row.names = TRUE)
+write.csv(cor_data, file.path(export_path, "FR.POL.GEN.cor_data.csv"), row.names = TRUE)
 
-# Melt the correlation data for use in ggplot
+# Créer la matrice de corrélation
 melted_cor_data <- melt(cor_data)
 
-# Create the correlation plot manually with ggplot2
 p <- ggplot(melted_cor_data, aes(Var2, Var1, fill = value)) +
   geom_tile(color = "white") + # Ajouter une bordure blanche pour mieux distinguer les cases
   geom_text(aes(label = round(value, 2)), color = "black", size = 5) +
@@ -198,14 +198,14 @@ p <- ggplot(melted_cor_data, aes(Var2, Var1, fill = value)) +
   coord_fixed()
 print(p)
 
-# Save the plot
+# Enregistrer
 ggsave(file.path(export_path, "Matrice_de_corrélations.png"), p, width = 12, height = 10, dpi = 300)
 
 
 
 
 
-### MODÈLES DE RÉGRESSIONS ###
+#### MODÈLES DE RÉGRESSIONS ####
 
 # Chargement des packages nécessaires
 library(modelsummary)
@@ -220,19 +220,19 @@ FR_POL_GEN_database$MP_total <- (FR_POL_GEN_database$MP_total - min(FR_POL_GEN_d
 FR_POL_GEN_database$MP_E_D <- (FR_POL_GEN_database$MP_E_D - min(FR_POL_GEN_database$MP_E_D)) / (max(FR_POL_GEN_database$MP_E_D) - min(FR_POL_GEN_database$MP_E_D)) * 100
 FR_POL_GEN_database$MP_center <- (FR_POL_GEN_database$MP_center - min(FR_POL_GEN_database$MP_center)) / (max(FR_POL_GEN_database$MP_center) - min(FR_POL_GEN_database$MP_center)) * 100
 
-# La variable 'pol' reste inchangée
+# Factorisation de la variable de l'appartenance politique ('pol')
 FR_POL_GEN_database$pol <- as.factor(FR_POL_GEN_database$pol)
 FR_POL_GEN_database$pol <- relevel(FR_POL_GEN_database$pol, ref = "droite")
 
-# Mise à jour du modèle pour inclure 'pol' après ajustement
+# Modèles OLS
 models <- list()
 models[['OLS1']] = lm(far_right_score ~ MP_center + lag(far_right_score, 1), data = FR_POL_GEN_database)
 models[['OLS2']] = lm(far_right_score ~ MP_center + MP_E_D + lag(far_right_score,1), data = FR_POL_GEN_database)
 models[['OLS3']] = lm(far_right_score ~ MP_center + MP_E_D + MP_total + lag(far_right_score,1), data = FR_POL_GEN_database)
 models[['OLS4']] = lm(far_right_score ~ MP_center + MP_E_D + MP_total + PPM_score + lag(far_right_score,1), data = FR_POL_GEN_database)
 models[['OLS5']] = lm(far_right_score ~ PPM_score + MP_center + MP_E_D + MP_total + president_score + lag(far_right_score,1), data = FR_POL_GEN_database)
-models[['OLS6']] = lm(far_right_score ~ MP_center + MP_E_D + MP_total + PPM_score + president_score + MP_center:MP_E_D + lag(far_right_score,1), data = FR_POL_GEN_database)
-models[['OLS7']] = lm(far_right_score ~ MP_E_D + MP_total + PPM_score + president_score + pol + lag(far_right_score,1), data = FR_POL_GEN_database)
+models[['OLS6']] = lm(far_right_score ~ socre_ed_1_tour + MP_center + MP_E_D + MP_total + PPM_score + president_score + MP_center:MP_E_D + lag(far_right_score,1), data = FR_POL_GEN_database)
+models[['OLS7']] = lm(far_right_score ~ socre_ed_1_tour + MP_E_D + MP_total + PPM_score + president_score + pol + lag(far_right_score,1), data = FR_POL_GEN_database)
 
 # Carte des coefficients pour l'affichage personnalisé
 cm <- c('(Intercept)' = '(Intercept)',
@@ -244,7 +244,8 @@ cm <- c('(Intercept)' = '(Intercept)',
         'president_score' = 'Score présidentiel',
         'polcentre' = 'Premier ministre du centre',
         'poldroite' = 'Premier ministre de droite',
-        'MP_center:MP_E_D' = 'N. députés centriste * N. députés ext.')
+        'MP_center:MP_E_D' = 'N. députés centriste * N. députés ext.',
+        'socre_ed_1_tour' = 'score_ed_1_tour')
 
 # Génération et affichage du résumé du modèle
 tab <- modelsummary(models,
@@ -272,11 +273,7 @@ print(doc, target = table_full_path)
 
 
 
-
-
-
-
-## CRÉATION DU GRAPHIQUE D'INTERACTION ##
+#### CRÉATION DU GRAPHIQUE D'INTERACTION ####
 
 library(ggplot2)
 library(dplyr)
@@ -293,13 +290,13 @@ models[['OLS4']] = lm(far_right_score ~ MP_center + MP_E_D + MP_total + PPM_scor
 models[['OLS5']] = lm(far_right_score ~ PPM_score + MP_center + MP_E_D + MP_total + president_score + lag(far_right_score,1), data = FR_POL_GEN_database)
 models[['OLS6']] = lm(far_right_score ~ MP_center + MP_E_D + MP_total + PPM_score + president_score + MP_center:MP_E_D + lag(far_right_score,1), data = FR_POL_GEN_database)
 
-# Extraction des coefficients pour OLS3
+# Extraction des coefficients pour OLS6
 coefficients_OLS6 <- coef(models[['OLS6']])
 
 # Création d'un dataframe pour les valeurs simulées
 simulation_data <- data.frame()
 
-# Niveaux MOD_FR pour la simulation
+# Niveaux du centre pour la simulation
 levels_MP_center <- seq(0, 100, by = 20)
 
 for(MP_center_val in levels_MP_center) {
@@ -316,9 +313,9 @@ for(MP_center_val in levels_MP_center) {
 # Affichage du graphique
 p <- ggplot(simulation_data, aes(x = MP_E_D, y = far_right_score, color = as.factor(MP_center))) +
   geom_line(size = 2) + 
-  labs(title = "Effet du nombre de députés d'extrême droite sur le score d'extrême droite en fonction du nombre de députés centristes",
+  labs(title = "Effet du nombre de députés d'extrême droite sur le SIED en fonction du nombre de députés centristes",
        x = "Nombre de députés d'extrême droite",
-       y = "Score d'extrême droite projeté",
+       y = "SIED projeté",
        color = "Niveaux du nombre de députés centristes") +
   theme_minimal() +
   theme(
@@ -331,7 +328,7 @@ p <- ggplot(simulation_data, aes(x = MP_E_D, y = far_right_score, color = as.fac
 print(p)
 
 
-# Export the plot
+# Enregistrer
 ggsave(filename = "Projections.pdf", plot = p, path = export_path, width = 12, height =10)
 
 
@@ -340,7 +337,7 @@ ggsave(filename = "Projections.pdf", plot = p, path = export_path, width = 12, h
 
 
 
-### CALCUL DES VIFS POUR LA VÉRIFICATION DE MULTICOLLINÉRATIRTÉ ###
+#### CALCUL DES VIFS POUR LA VÉRIFICATION DE MULTICOLLINÉRATIRTÉ ####
 
 
 # Installer et charger le package car pour utiliser la fonction vif()
